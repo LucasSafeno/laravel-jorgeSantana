89c2888c37b36f997a2f42bd091e37ab07b8f13b
Lucas Safeno <lugnorio@gmail.com>
2024-12-27 10:04:37 -0300

    Udemy - Laravel - Jorge Santana - 39. Nomeando rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -21,26 +21,25 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
-Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/login', function(){return 'Login';});
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
 
 //? Agrupamento de rotas - app
-
 Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {
-        return 'Clientes';});
+        return 'Clientes';})->name('app.clientes');
     
     Route::get('/fornecedores', function () {
-        return 'Fornecedores'; });
+        return 'Fornecedores'; })->name('app.fornecedores');
     
     Route::get('/produtos', function () {
-        return 'Produtos';});
+        return 'Produtos';})->name('app.produtos');
     
 });
 
