435cc542717b295689bb7e59ba985f47cbb31c74
Lucas Safeno <lugnorio@gmail.com>
2024-12-27 01:14:08 -0300

    Udemy - Laravel - Jorge Santana - 35. Tratando parâmetros de rotas com expressões regulares

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -19,13 +19,41 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
+
+
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}',
- function (string $nome, string $categoria, string $assunto, string $mensagem) {
-    echo "Estamos aqui - " . $nome." - Categoria  : ".$categoria.$assunto, $mensagem;
-});
+
+
+Route::get('/contato/{nome}/{categoria_id}', function (
+    string $nome,
+    int $categoria_id = 1
+    ) {
+
+    echo "Estamos aqui " . $nome . " - " . $categoria_id;
+    })->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+
+
+
+
+
+
+
+
+
+
+//? Valores padrões/opicionais devem ser declarados da direita para esquerda
+// Route::get(
+//     '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+//     function (
+//         string $nome        = 'Desconhecido',
+//         string $categoria   = 'Informação',
+//         string $assunto     = 'Contato',
+//         string $mensagem    = "mensagem não informada") 
+//         {
+//         echo "Estamos aqui - " . $nome . " - Categoria  : " . $categoria . $assunto . $mensagem;
+//         });
 
 Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
