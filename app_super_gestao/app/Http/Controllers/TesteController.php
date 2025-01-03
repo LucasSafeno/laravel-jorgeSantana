dd499773c49fc08b473e93e09c8233b93a847278
Lucas Safeno <lugnorio@gmail.com>
2025-01-03 11:02:49 -0300

    Udemy - Laravel - Jorge Sant'ana - 43. Encaminhando parâmetros do controlador para visualização

--- app_super_gestao/app/Http/Controllers/TesteController.php
+++ app_super_gestao/app/Http/Controllers/TesteController.php
@@ -8,6 +8,20 @@ class TesteController extends Controller
 {
     public function teste(int $p1,int $p2)
     {
-        echo "A soma de $p1 + $p2 é " . ($p1 + $p2);;
+        // echo "A soma de $p1 + $p2 é " . ($p1 + $p2);
+
+        //? Passar parametros do Controller para View
+        
+        //* Array
+        // return view('site.teste', [
+        //     'p1' => $p1,
+        //     'p2' => $p2
+        // ]);
+
+        //*Compact
+        //return view('site.teste', compact('p1', 'p2'));
+
+        //*With
+        return view ('site.teste')->with('p1', $p1)->with('p2', $p2);
     }
 }
