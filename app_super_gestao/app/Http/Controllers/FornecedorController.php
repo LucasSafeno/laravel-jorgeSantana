e76fd09273612b7d8471bcf25ecb2ea1a42c9b07
Lucas Safeno <lugnorio@gmail.com>
2025-01-07 17:33:23 -0300

    Udemy - Laravel - Jorge Sant'ana - 59. Blade - Variável loop

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -13,12 +13,34 @@ class FornecedorController extends Controller
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
                 'cnpj' => '00',
+                'ddd' => '81',
+                'telefone' => '0000000'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
                 'status' => 'S',
+                'cnpj' => '00',
+                'ddd' => '81',
+                'telefone' => '11111'
                 
             ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => '00',
+                'ddd' => '81',
+                'telefone' => '2222'
+                
+            ],
+            3 => [
+                'nome' => 'Fornecedor 4',
+                'status' => 'S',
+                'cnpj' => '00',
+                'ddd' => '81',
+                'telefone' => '33333'
+                
+            ],
+
 
         ];
 
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -4,16 +4,34 @@
 
 
 @isset($fornecedores)
-    Fornecedor : {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status :  {{ $fornecedores[0]['status'] }}
-    <br>
-    CNPJ : {{ $fornecedores[1]['cnpj'] ?? 'Dado não preenchido'}}
-    <!--
-        Se a vcriavel testada não estiver definida (isset)
-        ou
-        Se a variavel testada for null
-    !-->
-@endisset
+    @forelse($fornecedores as $indice => $fornecedor)
+         Interação Atual : {{ $loop->iteration}}
+         <br>
+        Fornecedor : {{ $fornecedor['nome']}}
+        <br>
+        Status :  {{ $fornecedor['status']}}
+        <br>
+        CNPJ : {{ $fornecedor['cnpj']}}
+        <br>
+        Telefone : ({{$fornecedor['ddd'] ?? ''}}) {{ $fornecedor['telefone'] ?? ''}}
+        <br>
 
+        @if($loop->first)
+            Primeira iteração do loop
+        @endif
+
+         @if($loop->last)
+            Última iteração do loop
+            <br>
+            Total de Registros : {{ $loop->count }}
+        @endif
+
+        <hr>
+
+    @empty
+        Não existe Fornecdores cadastrados!!
+    @endforelse
+
+
+@endisset
 
