de296eb01110e966ebaecef29ae1c029b81ea72b
Lucas Safeno <lugnorio@gmail.com>
2025-01-07 15:42:57 -0300

    Udemy - Laravel - Jorge Sant'ana - 48. Blade - @unless

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -4,7 +4,7 @@
 
 {{--@dd($fornecedores)--}}
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+{{-- @if(count($fornecedores) > 0 && count($fornecedores) < 10)
     
         <h3>Existem alguns fornecedores cadastrados</h3>
     
@@ -15,4 +15,20 @@
     @else 
         <h3>Ainda não existem fornecedores cadastrados</h3>
 
-@endif
+@endif --}}
+{{-- unless -> executa se o retorno for FALSE --}}
+
+{{-- @dd($fornecedores) --}}
+
+Fornecedor : {{ $fornecedores[0]['nome'] }}
+<br>
+Status :  {{ $fornecedores[0]['status'] }}
+<br>
+
+{{-- @if($fornecedores[0]['status'] == 'N')
+    Fornecedor Inativo
+@endif --}}
+
+@unless($fornecedores[0]['status'] == 'S') <!-- executa se o retorno da condição for false !-->
+    Fornecedor Inativo
+@endunless
