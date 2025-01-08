590615e26a8f4fa7d3e86bf6da4eb6e85a142ae0
Lucas Safeno <lugnorio@gmail.com>
2025-01-08 15:40:24 -0300

    Udemy - Laravel - Jorge Sant'ana - 68. Enviando o formulário de contato via POST

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato',[\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
 Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
