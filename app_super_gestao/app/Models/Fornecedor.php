f10e01aa84672bad43d22c8648b7ca656f116fb9
Lucas Safeno <lugnorio@gmail.com>
2025-01-20 17:31:39 -0300

    Udemy - Laravel - Jorge Sant'ana  95. Eloquent - Inserindo registros com Create e Fillable

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -10,4 +10,6 @@ class Fornecedor extends Model
      * ! Mudar  nome da tabela ( setar caso a tabela esteja com nome incorreto na hora de usar tinker)
      */
     protected $table = 'fornecedores';
+
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
