5bdce9293cd2b70ae5924bfff9d74906a495fd59
Lucas Safeno <lugnorio@gmail.com>
2025-01-20 17:09:42 -0300

    Udemy - Laravel - Jorge Sant'ana  - 94. Eloquent - Ajustando o nome da tabela no Model para um correto ORM

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    /**
+     * ! Mudar  nome da tabela ( setar caso a tabela esteja com nome incorreto na hora de usar tinker)
+     */
+    protected $table = 'fornecedores';
 }
--- app_super_gestao/app/Models/SiteContato.php
+++ app_super_gestao/app/Models/SiteContato.php
@@ -4,7 +4,4 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SiteContato extends Model
-{
-    //
-}
+class SiteContato extends Model {}
