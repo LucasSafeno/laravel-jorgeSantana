b574c1fd4594e2eaa05e5a59eb4f63737ece07d1
Lucas Safeno <lugnorio@gmail.com>
2025-03-19 09:51:05 -0300

    134. Customizando a apresentação dos erros de validação parte 1

--- app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,23 +1,36 @@
 {{$slot}}
 <form action={{route('site.contato')}} method="POST">
     @csrf
-    <input name="name"  value="{{old('name')}}" type="text" placeholder="Nome" class="{{$classe}}">
+    <input name="name" value="{{old('name')}}" type="text" placeholder="Nome" class="{{$classe}}">
     <br>
     <input name="telefone" value="{{old('telefone')}}" type="text" placeholder="Telefone" class="{{$classe}}">
     <br>
     <input name="email" value="{{old('email')}}" type="text" placeholder="E-mail" class="{{$classe}}">
     <br>
-    
+
     <select name="motivo_contatos_id" class="{{$classe}}">
         <option value="">Qual o motivo do contato?</option>
 
         @foreach ($motivos_contatos as $motivo_contato->id => $motivo_contato )
-            <option value="{{$motivo_contato->id}}" {{old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : ''}}>{{$motivo_contato->motivo_contato}}</option>
-            
+        <option value="{{$motivo_contato->id}}" {{old('motivo_contatos_id')==$motivo_contato->id ? 'selected' :
+            ''}}>{{$motivo_contato->motivo_contato}}</option>
+
         @endforeach
     </select>
     <br>
-    <textarea name="mensagem" class="{{$classe}}">{{ (old('mensagem' != '')) ? old('mensagem') : 'Preencha aqui sua mensagem'  }}</textarea>
+    <textarea name="mensagem"
+        class="{{$classe}}">{{ (old('mensagem' != '')) ? old('mensagem') : 'Preencha aqui sua mensagem'  }}</textarea>
     <br>
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
+
+
+@if($erros->any())
+<div style="position: absolute; top: 0px; left: 0px; width: 100%; background-color: red;">
+    @foreach ($errors->all() as  $erro)
+        {{ $erro}}
+        <br>
+    @endforeach
+</div>
+
+@endif
