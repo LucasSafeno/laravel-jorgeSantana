0c76bc632fb74348c9176e70f96de0cec5d99cb6
Lucas Safeno <lugnorio@gmail.com>
2025-03-19 10:08:25 -0300

    135. Customizando a apresentação dos erros de validação parte 2

--- app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,11 +1,18 @@
 {{$slot}}
 <form action={{route('site.contato')}} method="POST">
     @csrf
+
     <input name="name" value="{{old('name')}}" type="text" placeholder="Nome" class="{{$classe}}">
+    @if ($errors->has('name'))
+        {{$errors->first('name')}}
+    @endif
     <br>
+
     <input name="telefone" value="{{old('telefone')}}" type="text" placeholder="Telefone" class="{{$classe}}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
     <input name="email" value="{{old('email')}}" type="text" placeholder="E-mail" class="{{$classe}}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
 
     <select name="motivo_contatos_id" class="{{$classe}}">
@@ -17,9 +24,11 @@
 
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
     <br>
     <textarea name="mensagem"
         class="{{$classe}}">{{ (old('mensagem' != '')) ? old('mensagem') : 'Preencha aqui sua mensagem'  }}</textarea>
+        {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
     <button type="submit" class="{{$classe}}">ENVIAR</button>
 </form>
@@ -28,7 +37,7 @@
 @if($erros->any())
 <div style="position: absolute; top: 0px; left: 0px; width: 100%; background-color: red;">
     @foreach ($errors->all() as  $erro)
-        {{ $erro}}
+        {{ $erro }}
         <br>
     @endforeach
 </div>
