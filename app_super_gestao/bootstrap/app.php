d82ffc32dac163626d41f033ebd3ed165095367a
Lucas Safeno <lugnorio@gmail.com>
2025-04-09 13:37:08 -0300

    142. Apelidando middlewares

--- app_super_gestao/bootstrap/app.php
+++ app_super_gestao/bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(LogAcessoMiddleware::class);
+        $middleware->alias([
+            'log.acesso' => LogAcessoMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -22,7 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
-    ->name('site.index');
+    ->name('site.index')
+    ->middleware('log.acesso');
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])
     ->name('site.contato');
