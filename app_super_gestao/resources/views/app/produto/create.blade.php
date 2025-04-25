dbe21272171d7d9f79a6b0e51b90069533731843
Lucas Safeno <lugnorio@gmail.com>
2025-04-25 13:17:07 -0300

    166. Implementando o cadastro de produtos parte 3 (store)

--- app_super_gestao/resources/views/app/produto/create.blade.php
+++ app_super_gestao/resources/views/app/produto/create.blade.php
@@ -20,7 +20,7 @@
         <div class=" informacao-pagina">
 
                 <div style="width:30%; margin-left: auto; margin-right: auto; ">
-                    <form action="" method="post">
+                    <form action="{{route('produto.store')}}" method="post">
                         @csrf
                         <input type="text" value="" name="nome" id="nome" class="borda-preta" placeholder="Nome">
 
