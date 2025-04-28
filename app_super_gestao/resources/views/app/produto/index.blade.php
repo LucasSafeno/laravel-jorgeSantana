352f4d1052bfda30593ce62f0a9322c5f95bb7d9
Lucas Safeno <lugnorio@gmail.com>
2025-04-28 17:59:02 -0300

    186. Extra - Exibindo mais informações do fornecedor

--- app_super_gestao/resources/views/app/produto/index.blade.php
+++ app_super_gestao/resources/views/app/produto/index.blade.php
@@ -24,7 +24,8 @@
                         <tr>
                             <th>Nome</th>
                             <th>Descrição</th>
-                            <th>Fornecedor</th>
+                            <th>Nome do Fornecedor</th>
+                            <th>Site do Fornecedor</th>
                             <th>Peso</th>
                             <th>Unidade ID</th>
                             <th>Comprimento</th>
@@ -41,8 +42,8 @@
                             <tr>
                                 <td>{{$produto->nome}}</td>
                                 <td>{{$produto->descricao}}</td>
-                                
                                 <td>{{$produto->fornecedor->nome}}</td>
+                                <td>{{$produto->fornecedor->site}}</td>
                                 <td>{{$produto->peso}}</td>
                                 <td>{{$produto->unidade_id}}</td>
                                 <td>{{$produto->produto_detalhe->comprimento ?? ''}}</td>
