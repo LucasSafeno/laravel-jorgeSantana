b3a9073f59ad7540298214120f5cbf562222e975
Lucas Safeno <lugnorio@gmail.com>
2025-04-28 12:46:44 -0300

    184. Eloquent ORM 1 para N - Criando o relacionamento entre Fornecedor e Produto

---
+++ app_super_gestao/database/migrations/2025_04_28_153313_alter_produtos_relacionamento_fornecedores.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Criando a coluna em produtos que vai receber a FK de fornecedores
+        Schema::table('produtos', function (Blueprint $table) {
+
+            // insere registreo de fornecores para estabelizar o relacionamento
+            $fornecedor_id = DB::table('fornecedores')->insertGetId([
+                'nome' => 'Fornecedor Padrão SG',
+                'site' => 'fornecedorpadraosg.com.br',
+                'uf' => 'PE',
+                'email' => 'contato@fornecedorpadraosg.com.br',
+            ]);
+            $table->unsignedBigInteger('fornecedor_id')->default($fornecedor_id)->after('id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropForeign('produtos_fornecedor_id_foreign');
+            $table->dropColumn('fornecedor_id');
+        });
+    }
+};
